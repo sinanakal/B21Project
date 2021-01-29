e1d2f90b3ac5973c8ff73bb83ce4b25bcf2532a3
sinan akalin <71410874+sinanakal@users.noreply.github.com>
2021-01-29 13:21:28 +0200

    PROJECT IS ALREADI IN REMOTE,COMMITING MORE CHANGES

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,12 @@
 public class Hello {
     //more stuff
-    //mac and windows users are happy
+    //mac and windows users are
+    // push ile gonderme
+
+    // i have already published on Github.com
+    //now i am ready to make commits.
+    //but do I need to share again ???
+    /// ABSOLUTELY NO
+    // IT'S TIME COMMIT AND JUST PUSH!~``~
+
 }
